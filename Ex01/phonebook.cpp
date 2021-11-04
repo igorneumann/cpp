2146c8762aace7c65275a3e492b790a3c1d76f41
Igor Neumann <igorneumann@gmail.com>
2021-11-04 10:22:17 -0700

    terminado Ex01

--- Ex01/phonebook.cpp
+++ Ex01/phonebook.cpp
@@ -1,32 +1,88 @@
 #include "phonebook.h"
 
-void ft_add (t_data	*data)
+void ft_add (contact *cnt)
 {
-	data->i = 1;
-	std::cout << "add function!\n";
+	cout << "Please enter the first name:\n";
+	cin >> cnt->fname;
+	cout << "Please enter the last name:\n";
+	cin >> cnt->lname;
+	cout << "Please enter the nickname:\n";
+	cin >> cnt->nick;
+	cout << "Please enter the phone number:\n";
+	cin >> cnt->phone;
+	cout << "Please enter the darkest secret:\n";
+	cin >> cnt->secret;
 }
 
-void ft_search (t_data	*data)
+void ft_search (contact	*cnt, int i)
 {
-	data->i = 0;
-	std::cout << "search function!\n";
+	char 	com = 0;
+
+	while (com < 48 || com > (i + 48))
+	{
+		ft_printl (cnt, i);
+		cout << "\nEnter the contact # you want to search\n";
+		cin >> com;
+		if (com < 48 || com > (i + 48))
+			cout << "\n Invalid contact #, try again.\n";
+	}
+	cnt += --i;
+	cnt->printc();
+}
+
+void ft_printl (contact	*cnt, int i)
+{
+	int j = 0;
+
+	cout << "\n#|FIRST NAME|LAST NAME | NICKNAME |  PHONE # | DARK SEC |\n" ;
+	while (j++ < i)
+	{
+		cout << j  << "|" << tench(cnt->fname) << "|" << tench(cnt->lname) << "|" << tench(cnt->nick) << "|" << tench(cnt->phone) << "|" << tench(cnt->secret) << "|\n";
+		cnt++;
+	}
+}
+
+string tench(string input)
+{
+	int i = 0;
+
+	string output = "          ";
+	while (input[i])
+	{
+		if (i < 10)
+			output[i] = input[i];
+		i++;
+	}
+	if (i > 10)
+		output[9] = 46;
+	return (output);
 }
 
 int	main ()
 {
 	char 	*com = new char;
-	t_data	*data = new t_data;
+	contact	cnt[8];
+	int	i = 0;
 
 	while (strcmp(com, "EXIT"))
 	{
 		com = new char;
-		std::cout << "WELCOME TO MY AWESOME PHONEBOOK\n";
-		std::cout << "ADD, SEARCH or EXIT?\n";
-		std::cin >> com;
+		cout << "WELCOME TO MY AWESOME PHONEBOOK\n";
+		cout << "ADD, SEARCH or EXIT?\n";
+		cin >> com;
 		if (!strcmp(com, "ADD"))
-			ft_add(data);
+		{
+			ft_add(&cnt[i]);
+			if (i < 7)
+				i++;
+		}
 		else if (!strcmp(com, "SEARCH"))
-			ft_search(data);
+		{
+			if (i > 0)
+				ft_search(cnt, i);
+			else
+				cout << "\nPhonebook is empty\n\n";
+		}
 	}
 	return 0;
 }
--- Ex01/phonebook.h
+++ Ex01/phonebook.h
@@ -14,27 +14,45 @@
 # define PHONEBOOK_H
 
 #include <iostream>
+#include <stdlib.h>
 #include <string>
+#include <string_view>
+#include <cstring>
 
-typedef struct s_data
-{
-	struct s_data	*prev;
-	struct s_data	*next;
-	int				i;
-}					t_data;
+using namespace std;
 
-typedef struct s_main
+class contact
 {
-	t_data			*data;
-	int				i;
-}			t_main;
+	// Access specifier
+	public:
+
+	// Data
+	string	fname;
+	string	lname;
+	string	nick;
+	string	phone;
+	string	secret;
+	int		i;
+
+	// Print Contact Function()
+	void printc()
+	{
+		cout << "\nFirst Name is: " << fname;
+		cout << "\nLast Name is: " << lname;
+		cout << "\nNickname is: " << nick;
+		cout << "\nPhone number is: " << phone;
+		cout << "\nDarkest Secret is: " << secret << "\n\n";
+	}
+};
 
 /*
 *** phonebook.c
 */
 
 int					main ();
-void 				ft_search (t_data	*data);
-void				ft_add (t_data	*data);
+void 				ft_search (contact *cnt, int i);
+void				ft_add (contact *cnt);
+void				ft_printl (contact	*cnt, int i);
+string				tench(string input);
 
 #endif
