98dc3d6126c10ea57b9be90e455f65cd6a0ab4ac
Igor <igorneumann@gmail.com>
2022-03-18 19:32:39 +0100

    terminado todo (creo)

--- M08/ex02/main.cpp
+++ M08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ineumann <ineumann@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 18:25:18 by ineumann          #+#    #+#             */
-/*   Updated: 2022/03/16 19:27:28 by ineumann         ###   ########.fr       */
+/*   Updated: 2022/03/17 18:30:08 by ineumann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,18 @@ int main()
     MutantStack<int> mstack;
     mstack.push(5);
     mstack.push(17);
-    // std::cout << mstack.top() << std::endl;
-    // mstack.pop();
-    // std::cout << mstack.size() << std::endl;
-    // mstack.push(3); mstack.push(5); mstack.push(737); //[...] mstack.push(0);
-    // MutantStack<int>::iterator it = mstack.begin(); MutantStack<int>::iterator ite = mstack.end();
-    // ++it;
-    // --it;
-    // while (it != ite) 
-    // {
-    //     std::cout << *it << std::endl;
-    //     ++it; 
-    // }
-    // std::stack<int> s(mstack); return 0;
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    mstack.push(3); mstack.push(5); mstack.push(737); //[...] mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin(); MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite) 
+    {
+        std::cout << *it << std::endl;
+        ++it; 
+    }
+    std::stack<int> s(mstack); return 0;
     return 0;
 }
--- M08/ex02/mutantstack.hpp
+++ M08/ex02/mutantstack.hpp
@@ -6,7 +6,7 @@
 /*   By: ineumann <ineumann@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 18:25:10 by ineumann          #+#    #+#             */
-/*   Updated: 2022/03/16 19:52:22 by ineumann         ###   ########.fr       */
+/*   Updated: 2022/03/18 19:31:29 by ineumann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,60 @@
 #include <string>
 #include <iterator>
 #include <stack>
-
+#include <vector>
 
 template <typename T>
 class MutantStack : public std::stack<T>
 {
     public:
-        MutantStack( void );
-		MutantStack( const MutantStack &MutantStack );
-		~MutantStack( void );
-		MutantStack & operator = ( const MutantStack &MutantStack );
-
+        MutantStack( void ) : std::stack<T>() {};
+		MutantStack( const MutantStack &MutantStack ) : std::stack<T>(MutantStack)
+		{
+			if (this != &MutantStack)
+				*this = MutantStack;
+		};
+		~MutantStack( void ) { };
+		MutantStack & operator = ( const MutantStack &MutantStack )
+		{
+			std::stack<T>::operator=(MutantStack);
+ 			return *this;
+		};
 		typedef typename std::stack<T>::container_type::iterator iterator;
-		iterator	begin(void);
-		iterator	end(void);
+			iterator	begin(void)
+			{
+				return this->c.begin();
+			};
+			iterator	end(void)
+			{
+				return this->c.end();
+			};
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+			const_iterator	begin(void) const
+			{
+				return this->c.begin();
+			};
+			const_iterator	end(void) const
+			{
+				return this->c.end();
+			};
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+			reverse_iterator	rbegin(void)
+			{
+				return this->c.rbegin();
+			};
+			reverse_iterator	rend(void)
+			{
+				return this->c.rend();
+			};
+		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+			const_reverse_iterator	rbegin(void) const
+			{
+				return this->c.rbegin();
+			};
+			const_reverse_iterator	rend(void) const
+			{
+				return this->c.rend();
+			};
 };
 
 #endif
